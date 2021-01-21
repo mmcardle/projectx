a83433c300b336962bfb7d75f63373126d8e27ff
mmcardle <mark.mcardle@sohonet.com>
2021-01-21 16:40:33 +0000

    Try wait for DB

--- backend/config/start.sh
+++ backend/config/start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Wait For DB"
+/home/user/venv/bin/python /home/user/backend/app/manage.py wait_for_db
+
 echo "Applying any migrations"
 /home/user/venv/bin/python /home/user/backend/app/manage.py migrate
 
