57654868a2947225acb40a5419370da129fc7462
mmcardle <mark.mcardle@sohonet.com>
2021-01-22 11:26:29 +0000

    Tidy action config

--- backend/config/start.sh
+++ backend/config/start.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Wait For DB"
-/home/user/venv/bin/python /home/user/backend/app/manage.py wait_for_db
-
 echo "Applying any migrations"
 /home/user/venv/bin/python /home/user/backend/app/manage.py migrate
 
