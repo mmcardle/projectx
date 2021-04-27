1a574e41faedf56dfecb61d2c2fc83f0ee4e734f
Mark McArdle <mark@blueskyops.com>
2021-04-27 10:23:22 +0100

    Refactor FastAPI Routing generation

--- backend/app/api/routing.py
+++ backend/app/api/routing.py
@@ -4,72 +4,14 @@ from users.models import User
 
 # The API model for one object.
 from .fastapi import APIUser, MultipleAPIUsers, SingleAPIUser
-from .utils import check_api_key, get_user
+from .utils import get_user, add_list_route, add_get_route, add_create_route, add_update_route, add_delete_route
 
 router = APIRouter()
 
+config = {"name": "User"}
 
-@router.get(
-    "/users/",
-    summary="Retrieve a list of all the users.",
-    tags=["users"],
-    response_model=MultipleAPIUsers,
-    name="users-get",
-)
-def users_get(_: User = Depends(check_api_key)) -> MultipleAPIUsers:
-    models = User.objects.all()
-    return MultipleAPIUsers.from_qs(models)
-
-
-@router.post(
-    "/users/",
-    summary="Create a new user.",
-    tags=["users"],
-    response_model=SingleAPIUser,
-    name="users-post",
-)
-def user_post(api_user: APIUser, _: User = Depends(check_api_key)) -> SingleAPIUser:
-    user = api_user.new_user()
-    return SingleAPIUser.from_model(user)
-
-
-@router.get(
-    "/users/{user_uuid}/",
-    summary="Get a user.",
-    tags=["users"],
-    response_model=SingleAPIUser,
-    name="user-get",
-)
-def user_get(
-    user: User = Depends(get_user),
-    _: User = Depends(check_api_key),
-) -> SingleAPIUser:
-    return SingleAPIUser.from_model(user)
-
-
-@router.put(
-    "/users/{user_uuid}/",
-    summary="Update a user.",
-    tags=["users"],
-    response_model=SingleAPIUser,
-    name="user-put",
-)
-def user_put(
-    user: User = Depends(get_user),
-    api_user: APIUser = Body(...),
-    _: User = Depends(check_api_key),
-) -> SingleAPIUser:
-    return api_user.update_user(user)
-
-
-@router.delete(
-    "/users/{user_uuid}/",
-    summary="Delete a user.",
-    tags=["users"],
-    response_model=SingleAPIUser,
-    name="user-delete",
-)
-def user_delete(user: User = Depends(get_user), _: User = Depends(check_api_key)) -> SingleAPIUser:
-    api_user = SingleAPIUser.from_model(user)
-    user.delete()
-    return api_user
+add_list_route(router, User, MultipleAPIUsers, config=config)
+add_get_route(router, User, SingleAPIUser, get_user, config=config)
+add_create_route(router, User, APIUser, SingleAPIUser, config=config)
+add_update_route(router, User, SingleAPIUser, get_user, config=config)
+add_delete_route(router, User, SingleAPIUser, get_user, config=config)
--- backend/app/api/utils.py
+++ backend/app/api/utils.py
@@ -1,6 +1,7 @@
 from uuid import UUID
-
-from fastapi import Header, HTTPException, Path
+from pydantic import BaseModel
+from django.db import models
+from fastapi import APIRouter, Header, HTTPException, Path, Depends, Body
 
 from users.models import User
 
@@ -30,3 +31,99 @@ def get_user(
     if not instance:
         raise HTTPException(status_code=404, detail=f"Object {user_uuid} not found.")
     return instance
+
+
+
+def get_config_for_model(model, defaults):
+    # TODO - slugify name for urls
+    name = defaults.get("name", model.__name__)
+    name_plural = f"{name}s"
+    name_lower = defaults.get("name", model.__name__).lower()
+    name_lower_plural = f"{name_lower}s"
+    return name, name_plural, name_lower, name_lower_plural
+
+
+def add_list_route(router: APIRouter, model: models.Model, response_model: BaseModel, config: dict = {}):
+
+    name, name_plural, name_lower, name_lower_plural = get_config_for_model(model, config)
+
+    @router.get(
+        f"/{name_lower_plural}/",
+        summary=f"Retrieve a list of all the {name_plural}.",
+        tags=[f"{name_plural}"],
+        response_model=response_model,
+        name=f"{name_lower_plural}-get",
+    )
+    def _get(_: model = Depends(check_api_key)) -> response_model:
+        all_models = model.objects.all()
+        return response_model.from_qs(all_models)
+
+
+def add_get_route(router: APIRouter, model: models.Model, response_model: BaseModel, get_function, config: dict = {}):
+
+    name, name_plural, name_lower, name_lower_plural = get_config_for_model(model, config)
+
+    @router.get(
+        f"/{name_lower_plural}/{{uuid}}/",
+        summary=f"Get a {name}.",
+        tags=[f"{name_plural}"],
+        response_model=response_model,
+        name=f"{name_lower}-get",
+    )
+    def _get(
+        instance: response_model = Depends(get_function),
+        _: User = Depends(check_api_key),
+    ) -> response_model:
+        return response_model.from_model(instance)
+
+
+def add_create_route(router: APIRouter, model: models.Model, request_model: BaseModel, response_model: BaseModel, config: dict = {}):
+
+    name, name_plural, name_lower, name_lower_plural = get_config_for_model(model, config)
+
+    @router.post(
+        f"/{name_lower_plural}/{{uuid}}/",
+        summary=f"Create a new {name}.",
+        tags=[f"{name_plural}"],
+        response_model=response_model,
+        name=f"{name_lower_plural}-post",
+    )
+    def _post(api_instance: request_model, _: User = Depends(check_api_key)) -> response_model:
+        instance = api_instance.new_project()
+        return response_model.from_model(instance)
+
+
+def add_update_route(router: APIRouter, model: models.Model, response_model: BaseModel, get_function, config: dict = {}):
+
+    name, name_plural, name_lower, name_lower_plural = get_config_for_model(model, config)
+
+    @router.put(
+        f"/{name_lower_plural}/{{uuid}}/",
+        summary=f"Update a {name}.",
+        tags=[f"{name_plural}"],
+        response_model=response_model,
+        name=f"{name_lower}-put",
+    )
+    def _put(
+        instance: model = Depends(get_function),
+        api_instance: response_model = Body(...),
+        _: User = Depends(check_api_key),
+    ) -> response_model:
+        return response_model.update_project(instance)
+
+
+def add_delete_route(router: APIRouter, model: models.Model, response_model: BaseModel, get_function, config: dict = {}):
+
+    name, name_plural, name_lower, name_lower_plural = get_config_for_model(model, config)
+
+    @router.delete(
+        f"/{name_lower_plural}/{{uuid}}/",
+        summary=f"Delete a {name}.",
+        tags=[f"{name_plural}"],
+        response_model=response_model,
+        name=f"{name_lower}-delete",
+    )
+    def _delete(instance: model = Depends(get_function), _: User = Depends(check_api_key)) -> response_model:
+        api_instance = response_model.from_model(instance)
+        instance.delete()
+        return api_project
