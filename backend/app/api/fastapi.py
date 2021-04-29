a1dba99012008ab6ecd5cf1879af956e6754efcf
Mark McArdle <mark@blueskyops.com>
2021-04-29 11:39:41 +0100

    Make generic get_function use identifier

--- backend/app/api/fastapi.py
+++ backend/app/api/fastapi.py
@@ -1,12 +1,11 @@
+from typing import List
 from urllib import parse
 from uuid import UUID
-from typing import List
 
 from django.db import models
 from fastapi import Body, Depends, Header, HTTPException, Path
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 from pydantic_django import ModelSchema
-
 
 from users.models import User
 
@@ -24,121 +23,158 @@ def check_api_key(x_api_key: str = API_KEY_HEADER) -> User:
     return user
 
 
-def get_user(
-    user_uuid2: UUID = Path(..., description="The UUID of the user."),
-    x_api_key: str = Header(...),
-):
-    """
-    Retrieve the user from the given user UUID.
-    """
-    _ = check_api_key(x_api_key)
-    instance = User.objects.filter(public_uuid=user_uuid2).first()
-    if not instance:
-        raise HTTPException(status_code=404, detail=f"Object {user_uuid2} not found.")
-    return instance
+def schema_for_instance(django_model, fields):
+    class SingleSchema(ModelSchema):  # pylint: disable=too-few-public-methods
+        class Config:  # pylint: disable=too-few-public-methods
+            model = django_model
+            include = fields
+
+        @classmethod
+        def from_model(cls, instance: django_model):
+            """
+            Convert a Django model instance to an SingleSchema instance.
+            """
+            return cls(
+                public_uuid=instance.public_uuid,
+                email=instance.email,
+                first_name=instance.first_name,
+                last_name=instance.last_name,
+            )
+
+    return SingleSchema
 
 
-class RouteBuilder:  # pylint: disable=too-many-instance-attributes
+def schema_for_new_model(django_model, SingleSchema, fields):  # pylint: disable=invalid-name
+    class NewSchema(ModelSchema):
+        class Config:  # pylint: disable=too-few-public-methods
+            model = django_model
+            include = fields
+
+        def create_new(self):
+            """
+            Create a new Django model instance.
+            """
+            return django_model.create_new(
+                email=self.email,
+                first_name=self.first_name,
+                last_name=self.last_name,
+            )
+
+        def update(self, instance: django_model):
+            """
+            Update a Django model instance and return an SingleSchema instance.
+            """
+            instance.email = self.email or instance.email
+            instance.first_name = self.first_name or instance.first_name
+            instance.last_name = self.last_name or instance.last_name
+            instance.save()
+            return SingleSchema.from_model(instance)
+
+    return NewSchema
+
+
+def schema_for_multiple_models(SingleSchema):  # pylint: disable=invalid-name
+    class MultipleSchema(BaseModel):  # pylint: disable=too-few-public-methods
+        items: List[SingleSchema]
+
+        @classmethod
+        def from_qs(cls, qs):
+            """
+            Convert a Django queryset to a MultipleSchema instance.
+            """
+            return cls(items=[SingleSchema.from_model(i) for i in qs])
+
+    return MultipleSchema
+
+
+class RouteBuilder:
     def __init__(
         self,
-        django_model: models.Model,
-        django_model_identifier: str,
+        model: models.Model,
+        request_fields: List[str],
+        response_fields: List[str],
         config: dict = None,
     ) -> None:
-        self.django_model = django_model
-        self.django_model_identifier = django_model_identifier
+        self.model = model
         self.config = config if config else {}
-        self.name = self.config.get("name", self.django_model.__name__)
-        self.name_plural = f"{self.name}s"
-        self.name_lower = self.config.get("name", self.django_model.__name__).lower()
-        self.name_lower_plural = f"{self.name_lower}s"
-        self.path_prefix = f"/{parse.quote(self.name_lower_plural)}/"
-        self.path_for_list_and_post = self.path_prefix
-        self.path_for_identifer = self.path_prefix + "{" + django_model_identifier + "}/"
 
-        class NewSchema(ModelSchema):
-            class Config:  # pylint: disable=too-few-public-methods
-                model = self.django_model
-                include = ["email", "first_name", "last_name"]
+        self.new_instance_schema = schema_for_instance(model, response_fields)
+        self.multiple_instance_schema = schema_for_multiple_models(self.new_instance_schema)
+        self.instance_schema = schema_for_new_model(model, self.new_instance_schema, request_fields)
 
-            def create_new(self):
-                """
-                Create a new Django model instance.
-                """
-                return django_model.create_new(
-                    email=self.email,
-                    first_name=self.first_name,
-                    last_name=self.last_name,
-                )
+        self.get_function = self.get_identifier_function()
 
-            def update(self, instance: django_model):
-                """
-                Update a Django model instance and return an SingleSchema instance.
-                """
-                instance.email = self.email or instance.email
-                instance.first_name = self.first_name or instance.first_name
-                instance.last_name = self.last_name or instance.last_name
-                instance.save()
-                return SingleSchema.from_model(instance)
+    def get_identifier_function(self):
+        def _inner(
+            identifier: UUID = Path(..., description=f"The identifier of the {self.name}."),
+            x_api_key: str = Header(...),
+        ):
+            """
+            Retrieve the instance from the given model identifier.
+            """
+            _ = check_api_key(x_api_key)
+            instance = self.model.objects.filter(public_uuid=identifier).first()
+            if not instance:
+                raise HTTPException(status_code=404, detail=f"Object {identifier} not found.")
+            return instance
 
-        class SingleSchema(ModelSchema):
-            class Config:  # pylint: disable=too-few-public-methods
-                model = self.django_model
-                include = ["public_uuid", "email", "first_name", "last_name"]
+        return _inner
 
-            @classmethod
-            def from_model(cls, instance: self.django_model):
-                """
-                Convert a Django model instance to an SingleSchema instance.
-                """
-                return cls(
-                    public_uuid=instance.public_uuid,
-                    email=instance.email,
-                    first_name=instance.first_name,
-                    last_name=instance.last_name,
-                )
+    @property
+    def name(self):
+        return self.config.get("name", self.model.__name__)
 
-        class MultipleSchema(BaseModel):  # pylint: disable=too-few-public-methods
-            items: List[SingleSchema]
+    @property
+    def name_plural(self):
+        return f"{self.name}s"
 
-            @classmethod
-            def from_qs(cls, qs):
-                """
-                Convert a Django queryset to a MultipleSchema instance.
-                """
-                return cls(items=[SingleSchema.from_model(i) for i in qs])
+    @property
+    def name_lower(self):
+        return self.config.get("name", self.model.__name__).lower()
 
-        self.fast_api_model = NewSchema
-        self.fast_api_single_model = SingleSchema
-        self.fast_api_multiple_model = MultipleSchema
+    @property
+    def name_lower_plural(self):
+        return f"{self.name_lower}s"
+
+    @property
+    def path_prefix(self):
+        return f"/{parse.quote(self.name_lower_plural)}/"
+
+    @property
+    def path_for_list_and_post(self):
+        return self.path_prefix
+
+    @property
+    def path_for_identifer(self):
+        return self.path_prefix + "{identifier}/"
 
     def add_list_route_to_router(self, router):
         @router.get(
             self.path_for_list_and_post,
             summary=f"Retrieve a list of all the {self.name_plural}.",
             tags=[f"{self.name_plural}"],
-            response_model=self.fast_api_multiple_model,
+            response_model=self.multiple_instance_schema,
             name=f"{self.name_lower_plural}-get",
         )
-        def _get(_: User = Depends(check_api_key)) -> self.fast_api_multiple_model:
-            all_models = self.django_model.objects.all()
-            return self.fast_api_multiple_model.from_qs(all_models)
+        def _get(_: User = Depends(check_api_key)) -> self.multiple_instance_schema:
+            all_models = self.model.objects.all()
+            return self.multiple_instance_schema.from_qs(all_models)
 
         return _get
 
-    def add_get_route_to_router(self, router, get_function):
+    def add_get_route_to_router(self, router):
         @router.get(
             self.path_for_identifer,
             summary=f"Get a {self.name}.",
             tags=[f"{self.name_plural}"],
-            response_model=self.fast_api_single_model,
+            response_model=self.new_instance_schema,
             name=f"{self.name_lower}-get",
         )
         def _get(
-            instance: self.django_model = Depends(get_function),
+            instance: self.model = Depends(self.get_function),
             _: User = Depends(check_api_key),
-        ) -> self.fast_api_single_model:
-            return self.fast_api_single_model.from_model(instance)
+        ) -> self.new_instance_schema:
+            return self.new_instance_schema.from_model(instance)
 
         return _get
 
@@ -147,45 +183,44 @@ class RouteBuilder:  # pylint: disable=too-many-instance-attributes
             self.path_for_list_and_post,
             summary=f"Create a new {self.name}.",
             tags=[f"{self.name_plural}"],
-            response_model=self.fast_api_single_model,
+            response_model=self.new_instance_schema,
             name=f"{self.name_lower_plural}-post",
         )
-        def _post(api_instance: self.fast_api_model, _: User = Depends(check_api_key)) -> self.fast_api_single_model:
+        def _post(api_instance: self.instance_schema, _: User = Depends(check_api_key)) -> self.new_instance_schema:
             instance = api_instance.create_new()
-            return self.fast_api_single_model.from_model(instance)
+            return self.new_instance_schema.from_model(instance)
 
         return _post
 
-    def add_update_route_to_router(self, router, get_function):
+    def add_update_route_to_router(self, router):
         @router.put(
             self.path_for_identifer,
             summary=f"Update a {self.name}.",
             tags=[f"{self.name_plural}"],
-            response_model=self.fast_api_single_model,
+            response_model=self.new_instance_schema,
             name=f"{self.name_lower}-put",
         )
         def _put(
-            instance: self.django_model = Depends(get_function),
-            api_instance: self.fast_api_model = Body(...),
+            instance: self.model = Depends(self.get_function),
+            api_instance: self.instance_schema = Body(...),
             _: User = Depends(check_api_key),
-        ) -> self.fast_api_single_model:
+        ) -> self.new_instance_schema:
             return api_instance.update(instance)
 
         return _put
 
-    def add_delete_route_to_router(self, router, get_function):
+    def add_delete_route_to_router(self, router):
         @router.delete(
             self.path_for_identifer,
-            # "/users/{user_uuid}/",
             summary=f"Delete a {self.name}.",
             tags=[f"{self.name_plural}"],
-            response_model=self.fast_api_single_model,
+            response_model=self.new_instance_schema,
             name=f"{self.name_lower}-delete",
         )
         def _delete(
-            instance: self.django_model = Depends(get_function), _: User = Depends(check_api_key)
-        ) -> self.fast_api_single_model:
-            api_instance = self.fast_api_single_model.from_model(instance)
+            instance: self.model = Depends(self.get_function), _: User = Depends(check_api_key)
+        ) -> self.new_instance_schema:
+            api_instance = self.new_instance_schema.from_model(instance)
             instance.delete()
             return api_instance
 
--- backend/app/api/routing.py
+++ backend/app/api/routing.py
@@ -3,14 +3,16 @@ from fastapi import APIRouter
 from users.models import User
 
 # The API model for one object.
-from .fastapi import RouteBuilder, get_user
+from .fastapi import RouteBuilder
 
 router = APIRouter()
 
-route_builder = RouteBuilder(User, "user_uuid2")
+request_fields = ["email", "first_name", "last_name"]
+response_fields = ["public_uuid", "email", "first_name", "last_name"]
+route_builder = RouteBuilder(User, request_fields, response_fields)
 
 route_builder.add_list_route_to_router(router)
-route_builder.add_get_route_to_router(router, get_user)
+route_builder.add_get_route_to_router(router)
 route_builder.add_create_route_to_router(router)
-route_builder.add_update_route_to_router(router, get_user)
-route_builder.add_delete_route_to_router(router, get_user)
+route_builder.add_update_route_to_router(router)
+route_builder.add_delete_route_to_router(router)
