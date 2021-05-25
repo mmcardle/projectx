ef274b200930a4c8950045bda79ace2eafd15a2e
Mark McArdle <mark@blueskyops.com>
2021-05-25 18:55:43 +0100

    Fix config for CI

--- backend/config/start.sh
+++ backend/config/start.sh
@@ -5,14 +5,14 @@ echo "Applying any migrations"
 
 if [ -z "${DEBUG}" ]
 then
-    export UVICORN_ARGS="--uds /tmp/uvicorn\$(circus.wid).sock"
+    export UVICORN_ARGS="--uds /tmp/uvicorn.sock"
     export UVICORN_PROCESSES="4"
 else
     export UVICORN_ARGS="--port 8002 --reload"
     export UVICORN_PROCESSES="1"
 fi
 
-echo "Running Uvicorn with - ${UVICORN_ARGS} - ${UVICORN_PROCESSES}"
+echo "Running Uvicorn ${UVICORN_PROCESSES} processes with args - ${UVICORN_ARGS}"
 
 nginx -g "daemon on;"
 circusd /home/user/config/circus.ini
